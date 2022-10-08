d40e9fa9e688b11cbbd46fa5a99995b4f0b2170b
Lucas de Santis <93810444+LCSantis@users.noreply.github.com>
2022-10-08 17:42:30 -0300

    Desafio Concluído

--- Program.cs
+++ Program.cs
@@ -1,3 +1,12 @@
 ﻿using DesafioPOO.Models;
 
+Iphone i = new Iphone("135", "1111", "010101010101", 64);
+i.Ligar();
+i.ReceberLigacao();
+i.InstalarAplicativo("Telegram");
+
+Nokia n = new Nokia("246", "2222", "24242424242424", 128);
+n.Ligar();
+n.ReceberLigacao();
+n.InstalarAplicativo("Whatsapp");
 // TODO: Realizar os testes com as classes Nokia e Iphone
